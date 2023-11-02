fab3b7e1310d0a33ad9e627223f489e611569048
Евгений Ким <gr414_kie-@lab-404-4.tomtit-tomsk.ru>
2023-11-02 17:37:52 +0700

    second commit

--- js.js
+++ js.js
@@ -2,18 +2,46 @@
 let dateDisp = document.getElementById('week__content');
 let timeDisp = document.querySelectorAll('.time__content');
 let windowWidth = document.querySelector('.main__top').getBoundingClientRect().width;
-
+let dots = document.querySelectorAll('.time-dots')
+let timeSec = document.querySelectorAll('.time__section');
 let showDays;
-let allBlock = {
-    0:{
-        id: 2,
-        type: 'pole',
-        username: 'anastasia',
-        max: 8,
+
+let allBlock = [
+    {
+        day: 'ПТ',
+        id: 1,
+        type: 'Pole Dance',
+        username: 'Иванова Екатерина',
+        max: 2,
+    },
+]
+function chechWindowLenght(){
+    if(windowWidth >= 910){
+        showDays = 5;
     }
 }
-function chechWindowLenght(){
-    console.log(windowWidth);
+function createBlock(){
+    let newEllone = document.createElement('div');
+    let newElltwo = document.createElement('div');
+    let newPone = document.createElement('span');
+    let newPtwo = document.createElement('span');
+    let newPthr = document.createElement('span');
+    let newPfou = document.createElement('span');
+    newEllone.classList.add('block__cover');
+    newElltwo.classList.add('block');
+    newPone.innerHTML =  '09:00 - 10:00'
+    newPtwo.innerHTML = allBlock[0].type;
+    newPthr.innerHTML = allBlock[0].username;
+    if(allBlock[0].max !== 8){
+        newPfou.innerHTML = 'Свободно: '+allBlock[0].max+' из 8'
+    }
+    newElltwo.append(newPone)
+    newElltwo.append(newPtwo)
+    newElltwo.append(newPthr)
+    newElltwo.append(newPfou)
+
+    newEllone.appendChild(newElltwo);
+    return newEllone;
 }
 function getDayName(day,month,year, locale){   
     comp =  month+'/'+day+'/'+year;
@@ -41,13 +69,16 @@ for(let i = 0 ; i < 5 ; i++){
     dayone = String(dayone);
 }
 function loadDay(){
-    let noew = timeDisp[0].id;
+    let noew = timeDisp[0];
     for(let i = 0 ; i < 5 ; i++){
-        let newEll = document.createElement('div');
-        newEll.style.width = '155px';
-        newEll.style.height = '138px';
-        newEll.style.backgroundColor = '#000'
+        let newEll = createBlock();
         timeDisp[0].appendChild(newEll);
+    }
+    if(timeSec[0].offsetHeight == 164){
+        console.log(timeSec[0].offsetHeight);
+        dots[0].style.height = '151px';
+        dots[1].style.height = '151px';
+        console.log(dots);
     }
 }
 loadDay()
